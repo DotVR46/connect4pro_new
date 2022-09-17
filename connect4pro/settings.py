0081074aa79354ab2aded6552680f7b5ac93c82c
Baphomet <dmitrymakarov089@gmail.com>
2022-09-17 22:42:59 +0600

    Fix SECRET_KEY leak

--- connect4pro/settings.py
+++ connect4pro/settings.py
@@ -9,18 +9,18 @@ https://docs.djangoproject.com/en/4.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
-
+import os
 from pathlib import Path
-
+from dotenv import load_dotenv
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
-
+load_dotenv(os.path.join(BASE_DIR, ".env"))
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-f95qhlqyyjaadbzn4lwb7m)f$1x*#0i*r8km$%zn6laj2&v*qj"
+SECRET_KEY = os.environ.get("SECRET")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True

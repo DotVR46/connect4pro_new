f0b4f02372f395d0bd1f740a9fd6d3b3c454d73b
DotVR46 <maggot8950@gmail.com>
2022-12-12 16:52:30 +0600

    Adverts retrieve view, urls

--- apps/adverts/urls.py
+++ apps/adverts/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from apps.adverts.views import AdvertListView, AdvertUpdateView, AdvertCreateView
+from apps.adverts.views import AdvertListView, AdvertUpdateView, AdvertCreateView, AdvertRetrieveView
 
 urlpatterns = [
     path("adverts/list", AdvertListView.as_view(), name="advert-list"),
     path("adverts/update/<int:id>", AdvertUpdateView.as_view(), name="advert-update"),
-    path("adverts/create", AdvertCreateView.as_view(), name="advert-create")
+    path("adverts/create", AdvertCreateView.as_view(), name="advert-create"),
+    path("adverts/<int:id>", AdvertRetrieveView.as_view(), name="advert-get"),
 ]
